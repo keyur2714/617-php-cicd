94f8b21fc9d56e19cc6685fc4a0c7df09dc8aab0
keyur2714 <keyurjava27@gmail.com>
2023-04-05 07:54:29 +0530

    updated index.php

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <html>
 	<head>
+		<style>
+			h1{
+				color : red;	
+			}
+		</style>
 		<title>Welcome TO AWS CICD</title>
 	</head>
 	<body>
 		<h1>Hello Rade Krishna...!</h1>
+		<ul>
+			<li>Keyur</li>
+			<li>Denish</li>
+			<li>Vinit</li>
+		</ul>
 	</body>
 </html>
